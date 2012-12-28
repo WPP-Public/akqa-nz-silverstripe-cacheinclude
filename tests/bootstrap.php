ef2186dc65de1a4bafdf5e3341ccdd098551e79f
Cam Spiers <cameron@heyday.co.nz>
2012-12-28 14:18:10 +1300

    UPDATE: Silverstripe framework as dev dependancy

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,12 @@
 <?php
 
-$loader = require __DIR__ . '/../vendor/autoload.php';
-$loader->add('Heyday\CacheInclude\Tests', __DIR__);
+$dir = __DIR__ . '/..';
+
+if (file_exists($dir . '/vendor/autoload.php')) {
+
+	$loader = require_once $dir . '/vendor/autoload.php';
+
+	$loader->addClassMap(Composer\Autoload\ClassMapGenerator::createMap($dir . '/framework'));
+	$loader->add('Heyday\CacheInclude\Tests', __DIR__);
+
+}
