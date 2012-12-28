2b328325d72e010f52b0ca096bc83cc626e604d7
Cam Spiers <cameron@heyday.co.nz>
2012-12-28 14:30:46 +1300

    UPDATE: Getting ready for tests

--- tests/Heyday/CacheInclude/Tests/CacheIncludeTest.php
+++ tests/Heyday/CacheInclude/Tests/CacheIncludeTest.php
@@ -1,2 +1,11 @@
 <?php
 
+namespace Heyday\CacheInclude\Tests;
+
+class CacheIncludeTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSomething()
+    {
+        $this->assertTrue(true);
+    }
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,9 +4,9 @@ $dir = __DIR__ . '/..';
 
 if (file_exists($dir . '/vendor/autoload.php')) {
 
-	$loader = require_once $dir . '/vendor/autoload.php';
+    $loader = require_once $dir . '/vendor/autoload.php';
 
-	$loader->addClassMap(Composer\Autoload\ClassMapGenerator::createMap($dir . '/framework'));
-	$loader->add('Heyday\CacheInclude\Tests', __DIR__);
+    $loader->addClassMap(Composer\Autoload\ClassMapGenerator::createMap($dir . '/framework'));
+    $loader->add('Heyday\CacheInclude\Tests', __DIR__);
 
 }
