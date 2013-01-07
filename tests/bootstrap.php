1dee6decff147655a44e14766eb9c8882bdaa8bb
Cam Spiers <cameron@heyday.co.nz>
2013-01-08 12:27:59 +1300

    UPDATE: Jenkins config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,7 @@ $dir = __DIR__ . '/..';
 
 if (file_exists($dir . '/vendor/autoload.php')) {
 
-    $loader = require_once $dir . '/vendor/autoload.php';
+    $loader = require $dir . '/vendor/autoload.php';
 
     $loader->addClassMap(Composer\Autoload\ClassMapGenerator::createMap($dir . '/framework'));
     $loader->add('Heyday\CacheInclude\Tests', __DIR__);
