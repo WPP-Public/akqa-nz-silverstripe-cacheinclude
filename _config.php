cb16dc461c7b79cf8b4f7dc5ade62018c6031c2f
Cam Spiers <camspiers@gmail.com>
2013-04-22 00:34:46 +1200

    UPDATE: Don't add to data object and controllers by default

--- _config.php
+++ _config.php
@@ -4,10 +4,6 @@ if (file_exists(__DIR__ . '../vendor/autoload.php')) {
     require_once __DIR__ . '../vendor/autoload.php';
 }
 
-Object::add_extension('SiteTree', 'CacheIncludeSiteTreeDecorator');
-Object::add_extension('DataObject', 'CacheIncludeExtension');
-Object::add_extension('ContentController', 'CacheIncludeExtension');
-
 Director::addRules(20, array(
     'cache-include//$Action' => 'CacheIncludeController'
 ));
