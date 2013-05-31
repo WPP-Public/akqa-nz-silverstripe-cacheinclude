31eebc991708ee396251ce86641e9f2d0cf81884
Stephen <stephen@heyday.co.nz>
2013-05-31 13:25:15 +1200

    MINOR: Minor fix for silvesrtripe 3.0 compatibility

--- _config.php
+++ _config.php
@@ -4,7 +4,7 @@ if (file_exists(__DIR__ . '../vendor/autoload.php')) {
     require_once __DIR__ . '../vendor/autoload.php';
 }
 
-Director::addRules(20, array(
+Director::addRules(100, array(
     'cache-include//$Action' => 'CacheIncludeController'
 ));
 
