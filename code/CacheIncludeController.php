4d71973af80d0126cc0edae7e2095e57429985d4
Cam Spiers <camspiers@gmail.com>
2013-06-12 11:50:20 +1200

    UPDATE: Improve cacheing cli controller for clear cache actions

--- code/CacheIncludeController.php
+++ code/CacheIncludeController.php
@@ -5,8 +5,26 @@ use Heyday\CacheInclude\Container;
 /**
  * Class CacheIncludeController
  */
-class CacheIncludeController extends CliController
+class CacheIncludeController extends \CliController
 {
+    static $allowed_actions = array(
+        'index',
+        'clearAll',
+        'clearTemplate'
+    );
+    /**
+     * Holds the dependency injection container
+     * @var
+     */
+    protected $container;
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $this->container = Container::getInstance();
+        parent::__construct();
+    }
     /**
      * Ensure clear actions can only be run from the command line
      */
@@ -20,26 +38,38 @@ class CacheIncludeController extends CliController
     /**
      *
      */
+    public function index()
+    {
+        return <<<INFO
+Usage:
+sake cache-include #help
+sake cache-include/clearAll #clear all templates
+sake cache-include/clearTemplate/TemplateName #clear specific template
+
+INFO;
+    }
+    /**
+     * Clears all caches
+     */
     public function clearAll()
     {
-        $dic = $this->getContainer();
-        $dic['cacheinclude']->flushAll();
+        $this->getService('cacheinclude')->flushAll();
+        return 'Done' . PHP_EOL;
     }
     /**
      *
      */
     public function clearTemplate()
     {
-        if (isset($_GET['args'][0])) {
-            $dic = $this->getContainer();
-            $dic['cacheinclude']->flushByName($_GET['args'][0]);
+        if ($this->request->param('ID')) {
+            $this->getService('cacheinclude')->flushByName($this->request->param('ID'));
+            return 'Done' . PHP_EOL;
+        } else {
+            return 'You must specify a template:' . PHP_EOL . $this->index();
         }
     }
-    /**
-     * @return Container
-     */
-    protected function getContainer()
+    protected function getService($name)
     {
-        return Container::getInstance();
+        return $this->container[$name];
     }
 }
