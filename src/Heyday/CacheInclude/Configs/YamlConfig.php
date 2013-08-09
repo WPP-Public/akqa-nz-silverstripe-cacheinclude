0366b6a7543f4e170bbe8aa66912f03342007e0d
Stephen Holdaway <stecman@gmail.com>
2013-08-10 02:56:03 +1200

    MINOR: Revert part of previous commit (still fixes the original problem)

--- src/Heyday/CacheInclude/Configs/YamlConfig.php
+++ src/Heyday/CacheInclude/Configs/YamlConfig.php
@@ -18,17 +18,18 @@ class YamlConfig extends ArrayConfig
     public function __construct($yaml, Cache $cache = null)
     {
         if ($cache instanceof Cache) {
-            if (is_file($yaml)) {
+            if (strpos($yaml, "\n") === false && is_file($yaml)){
                 $yaml = file_get_contents($yaml);
-
-                if (!($result = $cache->load(md5($yaml)))) {
-                    $cache->save($result = Yaml::parse($yaml));
-                }
-            } else {
-                $result = array();
+            }
+            if (!($result = $cache->load(md5($yaml)))) {
+                $cache->save($result = Yaml::parse($yaml));
             }
         } else {
             $result = Yaml::parse($yaml);
+        }
+
+        if (!isset($result)) {
+            $result = array();
         }
 
         parent::__construct($result);
