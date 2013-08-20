b26ff6297e0b58e46b797353a78451ed1813e9c8
Cam Spiers <camspiers@gmail.com>
2013-08-20 15:03:18 +1200

    MINOR: Remove inclusion of autoloader because silverstripe already includes it

--- _config.php
+++ _config.php
@@ -1,9 +1,5 @@
 <?php
 
-if (file_exists(__DIR__ . '../vendor/autoload.php')) {
-    require_once __DIR__ . '../vendor/autoload.php';
-}
-
 if (isset($_GET['flush']) && $_GET['flush']) {
     define('CACHEINCLUDE_FORCE_EXPIRE', true);
 }
