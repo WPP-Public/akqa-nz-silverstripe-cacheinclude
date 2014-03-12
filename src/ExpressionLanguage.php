eb64e8c8ba584fa4b74c166cf5690f6a5da562c1
Cam Spiers <camspiers@gmail.com>
2014-03-12 15:10:11 +1300

    Remove closures from Expression language

--- src/ExpressionLanguage.php
+++ src/ExpressionLanguage.php
@@ -10,31 +10,60 @@ use Symfony\Component\ExpressionLanguage\ExpressionLanguage as SymfonyExpression
  */
 class ExpressionLanguage extends SymfonyExpressionLanguage
 {
-    /**
-     *
-     */
     protected function registerFunctions()
     {
         parent::registerFunctions();
 
         $this->register(
             'list',
-            function ($arg) {
-                return sprintf('%s::get()', $arg);
-            },
-            function (array $variables, $value) {
-                return \DataList::create($value);
-            }
+            array($this, 'listCompiler'),
+            array($this, 'listEvaluator')
         );
 
         $this->register(
             'instanceof',
-            function ($arg0, $arg1) {
-                return sprintf('%s instanceof %s', $arg0, $arg1);
-            },
-            function (array $variables, $arg0, $arg1) {
-                return $arg0 instanceof $arg1;
-            }
+            array($this, 'instanceofCompiler'),
+            array($this, 'instanceofEvaluator')
         );
     }
-} 
+
+    /**
+     * @param $arg
+     * @return string
+     */
+    public function listCompiler($arg)
+    {
+        return sprintf('%s::get()', $arg);
+    }
+
+    /**
+     * @param array $variables
+     * @param $value
+     * @return static
+     */
+    public function listEvaluator(array $variables, $value)
+    {
+        return \DataList::create($value);
+    }
+
+    /**
+     * @param $arg0
+     * @param $arg1
+     * @return string
+     */
+    public function instanceofCompiler($arg0, $arg1)
+    {
+        return sprintf('%s instanceof %s', $arg0, $arg1);
+    }
+
+    /**
+     * @param array $variables
+     * @param $arg0
+     * @param $arg1
+     * @return bool
+     */
+    public function instanceofEvaluator(array $variables, $arg0, $arg1)
+    {
+        return $arg0 instanceof $arg1;
+    }
+}
